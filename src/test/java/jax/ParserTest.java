fec566937f7e8402972d3004ce71850d109f7d06
Cavan <cavn2000@hotmail.com>
2026-01-27 11:06:51 +0800

    Added more Junit Tests

--- src/test/java/jax/ParserTest.java
+++ src/test/java/jax/ParserTest.java
@@ -3,6 +3,7 @@ package jax;
 import jax.command.Command;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ParserTest {
@@ -22,5 +23,27 @@ public class ParserTest {
         } catch (JaxException e) {
             assertEquals("Error - Invalid Input.", e.getMessage());
         }
+    }
+
+    @Test
+    public void parseTodo_emptyDescriptionException() {
+        String[] input = {"todo"};
+
+        JaxException thrown = assertThrows(JaxException.class, () -> {
+            Parser.parseTodo(input);
+        });
+
+        assertEquals("Error - Todo description cannot be empty.", thrown.getMessage());
+    }
+
+    @Test
+    public void parseDeadline_invalidFormatException() {
+        String[] input = {"deadline", "submit assignment /by tomorrow"};
+
+        JaxException thrown = assertThrows(JaxException.class, () -> {
+            Parser.parseDeadline(input);
+        });
+
+        assertEquals("Error - Invalid Date Format. Please use: yyyy-MM-dd (e.g., 2019-10-15 1800)", thrown.getMessage());
     }
 }
