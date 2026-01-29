fc05fd990538bb420c289e6cc92013a579f44436
Cavan <cavn2000@hotmail.com>
2026-01-29 12:07:39 +0800

    Tweaked code styles

--- src/main/java/jax/Jax.java
+++ src/main/java/jax/Jax.java
@@ -5,6 +5,9 @@ import jax.storage.Storage;
 import jax.task.TaskList;
 import jax.ui.Ui;
 
+/**
+ * Contains the chatbot run functions, encompassing interactive processes.
+ */
 public class Jax {
 
     private Storage storage;
